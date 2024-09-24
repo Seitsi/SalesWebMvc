03a36a3db1af7b83c193862db19ee6162cd75e30
Seitsi <seitsi2010@gmail.com>
2024-09-24 23:04:54 +0200

    Criando a model view controller Departamento

---
+++ SalesWebMvc/Controllers/DepartamentoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Eletrônico" });
+            list.Add(new Departamento { Id = 2, Nome = "Moda" });
+            return View(list);
+        }
+    }
+}
