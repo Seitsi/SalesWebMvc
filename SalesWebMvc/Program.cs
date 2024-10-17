d977c7ccd127b4e41103be16bf278c4f1d1361bd
Seitsi <seitsi2010@gmail.com>
2024-10-17 16:48:24 -0300

    DI LoginService

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<VendedorService>();
 builder.Services.AddScoped<DepartamentoService>();
-builder.Services.AddScoped<VendaService>();
+builder.Services.AddScoped<VendaService>(); 
+builder.Services.AddScoped<LoginService>();
 
 var enUS = new CultureInfo("pt-BR");
 var localizationOptions = new RequestLocalizationOptions
